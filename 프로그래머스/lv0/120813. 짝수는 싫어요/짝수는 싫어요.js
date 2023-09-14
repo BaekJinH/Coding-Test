a3aa034ce134d19ddba1a44cf2426efc0f734af4
BaekJinH <88886246+BaekJinH@users.noreply.github.com>
2023-09-14 14:28:42 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,9 +1,17 @@
-function solution (n) {
-    let array = [] ;
-    for ( let i = 0 ; i <= n ; i++ ) {
-        if ( i % 2 == 1 ) {
-            array.push(i)    
-        }
+function solution ( n ) {
+    let array = [ ] ;
+    for ( let i = 1; i <= n; i+=2 ) {
+        array.push(i)
     }
     return array
-}   
+}
+
+// function solution (n) {
+//     let array = [] ;
+//     for ( let i = 0 ; i <= n ; i++ ) {
+//         if ( i % 2 == 1 ) {
+//             array.push(i)    
+//         }
+//     }
+//     return array
+// }   
