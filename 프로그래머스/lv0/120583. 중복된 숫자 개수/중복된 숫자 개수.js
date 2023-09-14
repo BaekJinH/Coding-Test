3aeddd7c72c89924dc47c553e89fc0a86bc877f9
BaekJinH <88886246+BaekJinH@users.noreply.github.com>
2023-09-14 11:14:10 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,9 +1,16 @@
 function solution(array, n) {
-    let count = 0 ;
-    for ( let num of array ) {
-        if ( n === num ) {
-            count++
-        }
-    }
-    return count
+    var answer = 0;
+    let Array = array.filter((item) => item === n)
+    answer = Array.length
+
+    return answer;
 }
+// function solution(array, n) {
+//     let count = 0 ;
+//     for ( let num of array ) {
+//         if ( n === num ) {
+//             count++
+//         }
+//     }
+//     return count
+// }
