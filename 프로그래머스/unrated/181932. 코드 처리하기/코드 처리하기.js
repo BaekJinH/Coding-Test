44a2aaf24efadf9ef2143eb0c46fcf856dce46a8
BaekJinH <88886246+BaekJinH@users.noreply.github.com>
2023-09-15 14:53:31 +0900

    [unrated] Title: 코드 처리하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,18 @@
+function solution(code) {
+    let mode = 0
+    const retCode = code.split('').filter((value,idx)=>{
+        if(value === '1'){
+            mode = mode === 0 ? 1 : 0
+            return false
+        }
+
+        // mode === 1
+        if(mode){
+            return idx%2===1
+        }
+        // mode === 0
+        return idx%2===0
+    }).join('')
+    const answer =  retCode.length !== 0 ? retCode : "EMPTY";
+    return answer
+}
