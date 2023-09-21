b8f2e899c6ec3623738c60c9b9c5844474d024b8
BaekJinH <88886246+BaekJinH@users.noreply.github.com>
2023-09-21 17:01:41 +0900

    [unrated] Title: qr code, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181903. qr code/qr code.js
+++ 프로그래머스/unrated/181903. qr code/qr code.js
@@ -1,11 +1,15 @@
+// function solution(q, r, code) {
+//     let result = "";
+
+//     for (let i = 0; i < code.length; i++) {
+//         if (i % q === r) {
+//             result += code[i];
+//         }
+//     }
+
+//     return result;
+// }
+
 function solution(q, r, code) {
-    let result = "";
-
-    for (let i = 0; i < code.length; i++) {
-        if (i % q === r) {
-            result += code[i];
-        }
-    }
-
-    return result;
+  return [...code].filter((_, i) => i % q === r).join('');
 }
