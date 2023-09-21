21bd25178f9152c10d2740b3e396ebcc6b9b8002
BaekJinH <88886246+BaekJinH@users.noreply.github.com>
2023-09-21 11:09:12 +0900

    [unrated] Title: 공배수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181936. 공배수/공배수.js
@@ -0,0 +1,13 @@
+function solution(number, n, m) {
+    let answer ;
+    
+    if ( number % n == 0 &&  number % m == 0 ) {
+        answer = 1;
+    }
+    else {
+        answer = 0;
+    }
+    
+    return answer ; 
+        
+}
