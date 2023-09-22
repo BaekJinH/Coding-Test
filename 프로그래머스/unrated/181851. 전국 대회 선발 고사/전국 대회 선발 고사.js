911b15cfa75c7f6c8b629368622a034d1bd83616
BaekJinH <88886246+BaekJinH@users.noreply.github.com>
2023-09-22 14:00:25 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,17 +1,29 @@
-function solution(rank, attendance) {
-    const canAttend = attendance.map((can, idx) => can ? idx : -1).filter(idx => idx !== -1);
+// function solution(rank, attendance) {
+//     const canAttend = attendance.map((can, idx) => can ? idx : -1).filter(idx => idx !== -1);
     
-    canAttend.sort((a, b) => rank[a] - rank[b]);
+//     canAttend.sort((a, b) => rank[a] - rank[b]);
     
-    const top3 = canAttend.slice(0, 3);
+//     const top3 = canAttend.slice(0, 3);
+    
+//     return top3[0] * 10000 + top3[1] * 100 + top3[2];
+// }
+// 1st Code use to Method Channing
+function solution ( rank , attendance ) {
+    const top3 = attendance
+            .map((can , idx) => can ? idx : -1)
+            .filter( idx => idx !== -1 )
+            .sort((a,b) => rank[a] - rank[b])
+            .slice(0 , 3)
     
     return top3[0] * 10000 + top3[1] * 100 + top3[2];
+    
 }
 
-function solution(rank, attendance) {
-  const [a, b, c] = rank
-    .map((r, i) => [r, i])
-    .filter(([_, i]) => attendance[i])
-    .sort(([a], [b]) => a - b);
-  return 10000 * a[1] + 100 * b[1] + c[1];
-}
+
+// function solution(rank, attendance) {
+//   const [a, b, c] = rank
+//     .map((r, i) => [r, i])
+//     .filter(([_, i]) => attendance[i])
+//     .sort(([a], [b]) => a - b);
+//   return 10000 * a[1] + 100 * b[1] + c[1];
+// }
