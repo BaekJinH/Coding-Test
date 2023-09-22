afbdf03ead10462b0fdb1dbcc2185fef9fe69643
BaekJinH <88886246+BaekJinH@users.noreply.github.com>
2023-09-22 09:39:11 +0900

    [unrated] Title: 배열 조각하기, Time: 0.22 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.js
@@ -1,10 +1,24 @@
+// function solution(arr, query) {
+//     for (let i = 0; i < query.length; i++) {
+//         if (i % 2 === 0) {  // Even index
+//             arr = arr.slice(0, query[i] + 1);
+//         } else {  // Odd index
+//             arr = arr.slice(query[i]);
+//         }
+//     }
+//     return arr;
+// }
+
+
 function solution(arr, query) {
-    for (let i = 0; i < query.length; i++) {
-        if (i % 2 === 0) {  // Even index
-            arr = arr.slice(0, query[i] + 1);
-        } else {  // Odd index
-            arr = arr.slice(query[i]);
+    let index = 0;  
+    for (let q of query) {
+        if (index % 2 === 0) {  
+            arr = arr.slice(0, q + 1);
+        } else {  
+            arr = arr.slice(q);
         }
+        index++;  
     }
     return arr;
 }
