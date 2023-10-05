e1eae9c53f8db7906f5dd8fc4091414634f280d0
BaekJinH <88886246+BaekJinH@users.noreply.github.com>
2023-10-05 11:03:09 +0900

    [unrated] Title: 수 조작하기 1, Time: 3.36 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,17 +1,30 @@
-function solution(n, control) {
-    for ( let i in control ) {
-        if ( "w" == control[i] ) {
-            n += 1 ;
-        }
-        if ( "s" == control[i] ) {
-            n -= 1 ;    
-        }
-        if ( "d" == control[i] ) {
-            n += 10 ;    
-        }
-        if ( "a" == control[i] ) {
-            n -= 10 ;    
-        }
+function solution ( n , control ) {
+    const moves = {
+        'w' : 1,
+        's' : -1,
+        'd' : 10,
+        'a' : -10
+    }    
+    for ( let move of control ) {
+        n += moves[move]
     }
     return n
 }
+
+// function solution(n, control) {
+//     for ( let i in control ) {
+//         if ( "w" == control[i] ) {
+//             n += 1 ;
+//         }
+//         if ( "s" == control[i] ) {
+//             n -= 1 ;    
+//         }
+//         if ( "d" == control[i] ) {
+//             n += 10 ;    
+//         }
+//         if ( "a" == control[i] ) {
+//             n -= 10 ;    
+//         }
+//     }
+//     return n
+// }
