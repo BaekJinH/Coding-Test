897553834f671fb3afbbe71317184f3ee894a197
BaekJinH <88886246+BaekJinH@users.noreply.github.com>
2023-10-16 09:39:35 +0900

    [level unrated] Title: 카운트 업, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.js
@@ -0,0 +1,7 @@
+function solution(start_num, end_num) {
+    let result = []; // 배열 초기화
+    for (let i = start_num; i <= end_num; i++) {
+        result.push(i); // 올바른 push 메소드 호출
+    }
+    return result;
+}
