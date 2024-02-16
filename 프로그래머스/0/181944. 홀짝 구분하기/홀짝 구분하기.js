362f09d55aae5fdf649dc48774fa4d8f6b0c44f2
BaekJinH <88886246+BaekJinH@users.noreply.github.com>
2024-02-16 16:54:20 +0900

    [level 0] Title: 홀짝 구분하기, Time: 46.49 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.js
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -2,20 +2,7 @@ const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
-
-let input = [];
-
-rl.on('line', function (line) {
-    input = line.split(' ');
-}).on('close', function () {
-    n = Number(input[0]);
-    let result = 'even';
-    if ( n % 2 == 0 ) {
-        
-    }
-    if ( n % 2 == 1 ) {
-        result = 'odd' ;
-    }
-    console.log(`${n} is ${result}`)
-});
+}).on('line', function (line) {
+    const result = Number(line) % 2 ? 'odd' : 'even'
+    console.log(line, 'is', result)
+})
